257892368bc4e8b3eb1b2375b3dbe50edb768cc2
Daniela <danielasf2002@outlook.es>
2025-07-19 00:12:32 -0500

    Se crearon las funciones para procesar los datos y ver los resultados

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from utils import limpiar_datos
+from utils import cargar_datos, limpiar_datos
 from analisis_numpy import calcular_estadisticas
 from analisis_pandas import (
     mostrar_estadisticas_generales,
@@ -7,26 +7,36 @@ from analisis_pandas import (
     filtrar_por_fecha,
     ventas_por_categoria,
     filtrar_dulces_oct_dic,
-    mostrar_ventas_por_categoria
+    mostrar_ventas_por_categoria,
+    convertir_a_lista_productos
 )
+# se crearon las funciones: para procesar los datos y otra para generar los resultados.
+def procesar_datos(ventas):
+    ventas = limpiar_datos(ventas)
+    ventas["Fecha"] = pd.to_datetime(ventas["Fecha"])
+    return ventas
 
+def generar_resultados(ventas):
+    ventas_np = ventas['Total_Venta'].to_numpy()
+    estadisticas = calcular_estadisticas(ventas_np)
+    lista_productos = convertir_a_lista_productos(ventas)
 
-ventas = pd.read_csv('PF_GRUPO.csv')
-ventas = limpiar_datos(ventas)
-ventas["Fecha"] = pd.to_datetime(ventas["Fecha"])
+    print("\n-----------ANÁLISIS ESTADÍSTICOS DE LAS VENTAS---------------")
+    for clave, valor in estadisticas.items():
+        print(f"{clave.capitalize()}: {valor:.2f}")
 
-ventas_np = ventas['Total_Venta'].to_numpy()
-estadisticas = calcular_estadisticas(ventas_np)
+    mostrar_estadisticas_generales(ventas)
+    filtrar_poco_vendidos(ventas)
+    filtrar_por_fecha(ventas, mes=8, anio=2025)
+    filtrar_dulces_oct_dic(ventas)
+    mostrar_ventas_por_categoria(ventas)
+    ventas_por_categoria(ventas)
 
-# Análisis con NumPy
-print("\n-----------ANÁLISIS ESTADÍSTICOS DE LAS VENTAS---------------")
-for clave, valor in estadisticas.items():
-    print(f"{clave.capitalize()}: {valor:.2f}")
+    print("\nProductos en lista:")
+    for producto in lista_productos[:5]:  # Muestra solo los primeros 5
+        print(f"{producto.nombre} - {producto.total_venta():.2f}")
 
-# Análisis con Pandas
-mostrar_estadisticas_generales(ventas)
-filtrar_poco_vendidos(ventas)
-filtrar_por_fecha(ventas, mes=8, anio=2025)
-filtrar_dulces_oct_dic(ventas)
-mostrar_ventas_por_categoria(ventas)
-ventas_por_categoria(ventas)
+if __name__ == "__main__":
+    ventas = cargar_datos('PF_GRUPO.csv')
+    ventas = procesar_datos(ventas)
+    generar_resultados(ventas)
